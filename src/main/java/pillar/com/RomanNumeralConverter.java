01a284812fd991b66c4413539d70049ffbcfa178
wujo635 <wujo635@gmailc.om>
2019-02-18 11:54:33 -0500

    Pass convert 5 to V

--- src/main/java/pillar/com/RomanNumeralConverter.java
+++ src/main/java/pillar/com/RomanNumeralConverter.java
@@ -4,6 +4,9 @@ public class RomanNumeralConverter {
 
     public String convert(Integer i) {
         String output = new String();
+        if (i == 5){
+            return "V";
+        }
         for (int j = 0; j < i; j++) {
             output += "I";
         }
