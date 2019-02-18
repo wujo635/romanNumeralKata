790f0cf6c410decd26881152d5025b52afe24e82
wujo635 <wujo635@gmailc.om>
2019-02-18 11:42:36 -0500

    Fail convert 2 to II

--- src/test/java/pillar/com/RomanNumeralConverterTest.java
+++ src/test/java/pillar/com/RomanNumeralConverterTest.java
@@ -11,4 +11,9 @@ public class RomanNumeralConverterTest {
     public void shouldConvert1toI() {
         assertEquals("I", converter.convert(1));
     }
+
+    @Test
+    public void shouldConvert2toII() {
+        assertEquals("II", converter.convert(2));
+    }
 }
