1333ef0f4f18e342cb6c0e0ab617147142a2cb00
wujo635 <wujo635@gmailc.om>
2019-02-18 18:50:12 -0500

    Pass major Roman numerals test

--- src/main/java/pillar/com/RomanNumeralConverter.java
+++ src/main/java/pillar/com/RomanNumeralConverter.java
@@ -1,8 +1,25 @@
 package pillar.com;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumeralConverter {
 
+    private static final Map<Integer, String> majorUnitsMap = new HashMap<Integer, String>();
+    static {
+        majorUnitsMap.put(1,"I");
+        majorUnitsMap.put(5,"V");
+        majorUnitsMap.put(10,"X");
+        majorUnitsMap.put(50,"L");
+        majorUnitsMap.put(100,"C");
+        majorUnitsMap.put(500,"D");
+        majorUnitsMap.put(1000,"M");
+    }
+
     public String convert(Integer i) {
+        if (majorUnitsMap.containsKey(i)) {
+            return majorUnitsMap.get(i);
+        }
         String output = new String();
         int countdown = i;
         while (countdown >= 9) {
