bb36e96229fcf7fb6377d5b000c280d89192f60a
wujo635 <wujo635@gmailc.om>
2019-02-18 13:14:57 -0500

    Pass convert 10 to X

--- src/main/java/pillar/com/RomanNumeralConverter.java
+++ src/main/java/pillar/com/RomanNumeralConverter.java
@@ -1,20 +1,41 @@
 package pillar.com;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumeralConverter {
+
+    private static final Map<Integer, String> majorUnitsMap = new HashMap<Integer, String>();
+    static {
+        majorUnitsMap.put(1,"I");
+        majorUnitsMap.put(5,"V");
+        majorUnitsMap.put(10,"X");
+    }
 
     public String convert(Integer i) {
         String output = new String();
-        int fives = i / 5;
-        int notFive = i % 5;
-        for (int j = 0; j < fives; j++) {
-            output += "V";
-        }
-        if (notFive == 4) {
-            output = "IV";
-        } else {
-            for (int j = 0; j < notFive; j++) {
-                output += "I";
+        int countdown = i;
+        while (countdown >= 9) {
+            if (countdown == 9) {
+                output += "IX";
+                countdown -= 9;
+            } else {
+                output += "X";
+                countdown -= 10;
             }
+        }
+        while (countdown >= 4) {
+            if (countdown == 4) {
+                output += "IV";
+                countdown -= 4;
+            } else {
+                output += "V";
+                countdown -= 5;
+            }
+        }
+        while (countdown > 0) {
+            output += "I";
+            countdown--;
         }
         return output;
     }
