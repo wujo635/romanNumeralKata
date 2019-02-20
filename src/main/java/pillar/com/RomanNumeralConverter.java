82d979e2b95c42a02a726993746ac9ba1bdd1ed0
wujo <wujo635@gmail.com>
2019-02-19 23:21:19 -0500

    Refactor checking special input cases to separate method

--- src/main/java/pillar/com/RomanNumeralConverter.java
+++ src/main/java/pillar/com/RomanNumeralConverter.java
@@ -17,11 +17,8 @@ public class RomanNumeralConverter {
     }
 
     public String convert(Integer i) {
-        if (i == 0) {
-            return "Nulla";
-        } else if (i < 0 || i > 3000) {
-            return "Failed to convert input.";
-        }
+        String x = isUnconvertable(i);
+        if (x != null) return x;
         if (majorUnitsMap.containsKey(i)) {
             return majorUnitsMap.get(i);
         }
@@ -31,6 +28,15 @@ public class RomanNumeralConverter {
             }
         }
         return new String(new char[i]).replace("\0","I");
+    }
+
+    private String isUnconvertable(Integer i) {
+        if (i == 0) {
+            return "Nulla";
+        } else if (i < 0 || i > 3000) {
+            return "Failed to convert input.";
+        }
+        return null;
     }
 
 }
